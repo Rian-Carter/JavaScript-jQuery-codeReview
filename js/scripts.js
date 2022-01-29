9136c62a3badd317dc094f4a25c3b33abca99710
Rian Carter <R.Carter86@hotmail.com>
2022-01-28 19:30:37 -0800

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   $("#surveyQuestions").submit(function(event) {
     event.preventDefault();
+    // $("#python").hide();
+    // $("#javascript").hide();
+    // $("#ruby").hide();
 
     const color = parseInt($("#color").val()); {
     }
     const animal = parseInt($("#animal").val()); {
-      alert("Please select a color");
+      // alert("Please select a color");
     }
     const shape = parseInt($("#shape").val()); {
     }
@@ -14,11 +17,15 @@ $(document).ready(function() {
     const country = parseInt($("#country").val()); {
     }
     const score = color + animal + shape + activity + country;
+
+
     if (score >= 1 && score <= 5) {
       $("#python").show();
     } else if (score >= 6 && score <= 10) {
       $("#javascript").show();
-    } else (score >= 11 && score <= 15) {
+    } else {
       $("#ruby").show();
     }
   });
+});
+
