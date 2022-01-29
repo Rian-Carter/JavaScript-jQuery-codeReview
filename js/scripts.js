7d2dce67f88f0e88d644d90b37673a148291d5a3
Rian Carter <R.Carter86@hotmail.com>
2022-01-28 19:21:39 -0800

    WIP currently struggle bussing

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,22 @@ $(document).ready(function() {
     event.preventDefault();
 
     const color = parseInt($("#color").val()); {
-
     }
     const animal = parseInt($("#animal").val()); {
       alert("Please select a color");
     }
     const shape = parseInt($("#shape").val()); {
-
     }
     const activity = parseInt($("#activity").val()); {
-
     }
     const country = parseInt($("#country").val()); {
-
     }
     const score = color + animal + shape + activity + country;
-  
     if (score >= 1 && score <= 5) {
       $("#python").show();
     } else if (score >= 6 && score <= 10) {
       $("#javascript").show();
-    } else (score )
-  
-    });
+    } else (score >= 11 && score <= 15) {
+      $("#ruby").show();
+    }
   });
