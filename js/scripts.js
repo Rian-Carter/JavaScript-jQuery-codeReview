8e843bafde64306c3043c36e5753a8c083304ac4
Rian Carter <R.Carter86@hotmail.com>
2022-01-28 18:52:02 -0800

    add const to scripts.js convert strings into intergers, and call an id

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-  $("#questions").submit(function(event) {
+  $("#surveyQuestions").submit(function(event) {
     event.preventDefault();
+
+    const color = parseInt($("#question1").val());
+
+    const animal = parseInt($("#question2").val());
+
+    const shape = parseInt($("#question3").val());
+
+    const activity = parseInt($("#question4").val());
+
+    const country = parseInt($("#question5").val());
+
+
     
   }
